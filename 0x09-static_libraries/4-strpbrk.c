f25cdce34c16f7bdc0157d41e791d1c87e4f1d54
buoyantichub <nsikandonatusekpo@gmail.com>
2023-08-05 03:19:21 -0700

    Updated

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,24 +1,23 @@
 #include "main.h"
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string to check
- * @accept: string to check against
- *
- * Return: pointer to byte in s that matches or NULL if no match
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+	int k;
 
-	for (i = 0; s[i]; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j]; j++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (s[i] == accept[j])
-				break;
+		if (*s == accept[k])
+		return (s);
 		}
-		if (accept[j])
-			return (s + i);
+	s++;
 	}
-	return (0);
+
+return ('\0');
 }
